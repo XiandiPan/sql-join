356f9239d2fe49e62e0b82f43b6347ae1b50ac9e
Tanya Shylock <tourtanya@gmail.com>
2023-04-25 17:39:31 -0600

    after code review

--- queries.sql
+++ queries.sql
@@ -1,1 +1,16 @@
--- write your queries here
+SELECT first_name, COUNT(*)
+    FROM owners
+    JOIN vehicles
+    ON owners.id = vehicles.owner_id
+    GROUP BY owners.id
+    ORDER BY first_name ASC;
+
+SELECT first_name, ROUND(AVG(price)), COUNT(owner_id)
+    FROM owners
+    JOIN vehicles
+    ON owners.id = vehicles.owner_id
+    GROUP BY owners.id
+    HAVING ROUND(AVG(price)) > 10000 AND COUNT(owner_id) > 1
+    ORDER BY first_name DESC;
+
+
--- vehicles.sql
+++ vehicles.sql
@@ -38,11 +38,4 @@ INSERT INTO vehicles (make, model, year, price, owner_id)
     ('BMW', '300', 2012, 22999.99, 6),
     ('BMW', '700', 2015, 52999.99, 6);
 
-    SELECT first_name, AVG(*)
-    FROM owners
-    JOIN vehicles
-    ON owners.id = vehicles.owner_id
-    GROUP BY first_name
-    ORDER BY first_name ASC
 
-    SELECT first_name
